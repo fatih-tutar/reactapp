1a1c828fb51f0fbad9e3b967dd580c277bacd847
fatih-tutar <fatihtutar93@gmail.com>
2022-03-24 10:57:38 +0300

    Javascript Expression

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 
 function App() {
+  const test = 10;
+  const isAuth = true;
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      { 1 + 1 }
+      <h4>{"Fatih".toUpperCase()}</h4>
+      <h4>{test}</h4>
+      <div>
+        {
+          isAuth ? <p>Kullanıcı Kayıtlı</p>
+          : null
+        }
+      </div>
     </div>
   );
 }
